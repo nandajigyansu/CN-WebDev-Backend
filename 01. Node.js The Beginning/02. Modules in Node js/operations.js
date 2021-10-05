072aaebaca100dccb4f7483348a149b1c6cc5b52
Jigyansu Nanda <89679161+nandajigyansu@users.noreply.github.com>
2021-10-05 19:38:12 +0530

    Create operations.js

---
+++ 01. Node.js The Beginning/02. Modules in Node js/operations.js
@@ -0,0 +1,18 @@
+
+// module keyword is redundant
+
+module.exports.add = function (a, b) {
+    return a + b;
+}
+
+module.exports.subtract = (a, b) => {
+    return a - b;
+}
+
+module.exports.multiply = function (a, b) {
+    return a * b;
+}
+
+module.exports.divide = (a, b) => {
+    return a / b;
+}
